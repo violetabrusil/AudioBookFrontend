92f553a053701f01f8b6deecc5a97fff66403dc9
Violeta <josselyn.brusil@epn.edu.ec>
2022-06-27 10:36:13 -0500

    Creación de componente para la autentcación de usuarios en el sistema web

--- audiobookapp/src/App.js
+++ audiobookapp/src/App.js
@@ -1,7 +1,11 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-import AudioBooksList from "../src/pages/audioBook/AudioBookList"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import AudioBooksList from "../src/pages/audioBook/AudioBookList";
 import AddAudioBook from "../src/pages/audioBook/AddAudioBook";
 import EditAudioBook from "../src/pages/audioBook/EditAudioBook";
+import Navbar from "../src/components/Navbar";
+import Home from "../src/pages/authentication/Home";
+import Login from "../src/pages/authentication/Login";
+import Register from "../src/pages/authentication/Register";
 
 
 function App() {
@@ -9,6 +13,10 @@ function App() {
     <Router>
       <div>
         <Switch>
+          <Route path="/" component={Home}></Route>
+          <Route path="/login" component={Login}></Route>
+          <Route path="/register" component={Register}></Route>
+          <Route path="/menu" component={Navbar}></Route>
           <Route path="/audioBookManager" component={AudioBooksList}></Route>
           <Route path="/addAudioBook" component={AddAudioBook}></Route>
           <Route path="/updateAudioBook/:idAudioBook/" component={EditAudioBook}></Route>
---
+++ audiobookapp/src/components/Navbar.js
@@ -0,0 +1,18 @@
+ import React from "react";
+ import { Link } from "react-router-dom";
+ import * as FaIcons from "react-icons/fa"
+
+ function Navbar() {
+    return (
+        <>
+        <div className="navbar">
+            <Link to="#" className='menu-bars'>
+                <FaIcons.FaBars/>
+            </Link>
+
+        </div>
+        </>
+    )
+ }
+
+ export default Navbar;
